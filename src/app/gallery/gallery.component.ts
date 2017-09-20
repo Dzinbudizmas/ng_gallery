c3fd5e6c1a2a29eb2547f338509f0ad14638b769
Dzinbudizmas <laimucka@gmail.com>
2017-09-21 00:18:11 +0300

    Gallery completed

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,36 +1,70 @@
 import {Component, OnInit} from '@angular/core';
 
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 const IMAGES: string[] = [
-  './assets/images/big1.jpg',
-  './assets/images/big2.jpg',
-  './assets/images/big3.jpg',
-  './assets/images/big4.jpg'
+  './assets/images/beetle1small.jpg',
+  './assets/images/beetle2small.jpg',
+  './assets/images/beetle3small.jpg',
+  './assets/images/beetle4small.jpg'
+];
+
+const IMAGESBIG: string[] = [
+  './assets/images/beetle1big.jpg',
+  './assets/images/beetle2big.jpg',
+  './assets/images/beetle3big.jpg',
+  './assets/images/beetle4big.jpg'
 ];
 
 @Component({
   selector: 'app-gallery',
   templateUrl: 'gallery.component.html',
-  styleUrls: ['gallery.component.css']
+  styleUrls: ['gallery.component.css'],
+  providers: [NgbCarouselConfig]
 })
 
 export class GalleryComponent implements OnInit{
   images = IMAGES;
+  bigImages = IMAGESBIG;
   selectedImage: string;
+  selectedIndex = 0;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, config: NgbCarouselConfig) {
+    config.interval = 0;
+  }
 
   onSelect(img: string): void {
-    this.selectedImage = img;
+    this.selectedIndex = this.images.findIndex(x => x === img);
+    this.selectedImage = img.replace('small', 'mid');
+  }
+
+  onNext(arrow: string): void {
+    if (arrow === 'up') {
+      if (this.selectedIndex === 0){
+        this.selectedIndex = this.images.length;
+      }
+      this.selectedIndex -= 1;
+      this.selectedImage = this.images[this.selectedIndex].replace('small', 'mid');
+    } else {
+      if (arrow === 'down') {
+        this.selectedIndex += 1;
+        if (this.selectedIndex === this.images.length) {
+          this.selectedIndex = 0;
+        }
+        this.selectedImage = this.images[this.selectedIndex].replace('small', 'mid');
+      }
+    }
   }
 
   open(content) {
-    this.modalService.open(content);
+    const options: NgbModalOptions = {
+      size: 'lg'
+    };
+    this.modalService.open(content, options);
   }
 
   ngOnInit(): void {
-    this.selectedImage = this.images[0];
+    this.onSelect(this.images[0]);
   }
-
 }
